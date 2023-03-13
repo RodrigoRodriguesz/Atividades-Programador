9b5c9bcdc6dc69d3781bb51d1c508d1c793c45d1
RodrigoRodriguesz <rodrigorodrigues469@gmail.com>
2023-03-13 16:44:10 -0300

    Create Estrutura.py

---
+++ Lista De Revisão/Estruturas Condicionais/Estrutura.py
@@ -0,0 +1,34 @@
+#Escreva um programa que solicite ao usuário um número e imprima se ele é positivo, negativo ou zero.
+
+# numero = float (input('Escreva um número:'))
+# if numero >0:
+#     print("positivo")
+# elif numero <0:
+#     print('Negativo')
+# else:
+#     print('igual:')
+
+#Escreva um programa que leia o nome e a idade de uma pessoa e imprima uma mensagem personalizada com base na idade. 
+# Se a idade for menor que 18 anos, imprima "Você é menor de idade". 
+# Se a idade estiver entre 18 e 65 anos, imprima "Você é adulto". Caso contrário, imprima "Você é idoso"
+
+# while True:
+#     try:
+
+#         nome = str( input("Digite seu Nome:"))
+#         idade=int(input("Qual sua idade:"))
+#         if idade <18 :
+#             print(nome , 'Voce é menor de idade')
+#         elif idade >=18 and idade <65: 
+#             print(nome , 'Voce é adulto')
+#         else:
+#             print(nome , 'Voce é idoso')
+#             break
+#     except:
+#         print('Digite algo válido: ')
+
+#Escreva um programa que solicite ao usuário a nota de um aluno em uma prova e
+# imprima a mensagem "Aprovado" 
+# se a nota for maior ou igual a 7, "Reprovado" 
+# se a nota for menor que 5 e "Recuperação" se a nota estiver entre 5 e 7.
+Usuario=input('Escreva seu nome:')
