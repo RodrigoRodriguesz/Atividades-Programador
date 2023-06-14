7f4a5d68c5f101bbfe949e9ca0f8da16e8d4a6f2
RodrigoRodriguesz <rodrigorodrigues469@gmail.com>
2023-06-14 11:41:02 -0300

    att

--- TrabalhoGp/home.py
+++ TrabalhoGp/home.py
@@ -5,16 +5,19 @@ os.environ['KIVY_GL_BACKEND']='angle_sdl2'
 
 from kivymd.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
-from View.main_screen.MainScreen import MainScreenView
+from View.Main_screen.MainScreen import MainScreenView
 
 class MainApp(MDApp):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.sm= MDScreenManager()
+        self.load_all_kv_files(self.directory)
 
     def build(self):
-        self.sm.add_widget(MainScreenView)
-
+        self.sm.add_widget(MainScreenView())
+        self.theme_cls.material_style = 'M3'
+        self.theme_cls.primary_palette= 'Orange'
+        self.theme_cls.theme_style = "Light"
         return self.sm
 
 MainApp().run()
